8c1408b83fee5c83340a0e3f2eae850c5f375fab
NguyenVanHien <nguyenvanhienbk2006@gmail.com>
2017-02-09 09:47:07 +0700

    add new table

--- spec/db/category.sql
+++ spec/db/category.sql
@@ -10,24 +10,24 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-02-08 17:14:00
+Date: 2017-02-09 09:44:42
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for tree
+-- Table structure for category
 -- ----------------------------
-DROP TABLE IF EXISTS `tree`;
-CREATE TABLE `tree` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `root` int(11) DEFAULT NULL,
   `lft` int(11) NOT NULL,
   `rgt` int(11) NOT NULL,
   `lvl` smallint(5) NOT NULL,
   `name` varchar(60) NOT NULL,
   `icon` varchar(255) DEFAULT NULL,
-  `icon_type` smallint(1) NOT NULL DEFAULT '1',
+  `icon_type` tinyint(1) NOT NULL DEFAULT '1',
   `active` tinyint(1) NOT NULL DEFAULT '1',
   `selected` tinyint(1) NOT NULL DEFAULT '0',
   `disabled` tinyint(1) NOT NULL DEFAULT '0',
@@ -41,16 +41,14 @@ CREATE TABLE `tree` (
   `removable` tinyint(1) NOT NULL DEFAULT '1',
   `removable_all` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `tree_NK1` (`root`),
-  KEY `tree_NK2` (`lft`),
-  KEY `tree_NK3` (`rgt`),
-  KEY `tree_NK4` (`lvl`),
-  KEY `tree_NK5` (`active`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  KEY `tbl_product_NK1` (`root`),
+  KEY `tbl_product_NK2` (`lft`),
+  KEY `tbl_product_NK3` (`rgt`),
+  KEY `tbl_product_NK4` (`lvl`),
+  KEY `tbl_product_NK5` (`active`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of tree
+-- Records of category
 -- ----------------------------
-INSERT INTO `tree` VALUES ('1', '1', '1', '4', '0', 'redsd', '', '1', '1', '0', '0', '0', '1', '0', '1', '1', '1', '1', '1', '0');
-INSERT INTO `tree` VALUES ('2', '1', '2', '3', '1', '2ffsdfdfds', '', '1', '1', '0', '0', '0', '1', '0', '1', '1', '1', '1', '1', '0');
-INSERT INTO `tree` VALUES ('3', '3', '1', '2', '0', '111111111111', '', '1', '1', '0', '0', '0', '1', '0', '1', '1', '1', '1', '1', '0');
+INSERT INTO `category` VALUES ('1', '1', '1', '2', '0', '212121', '', '1', '1', '0', '0', '0', '1', '0', '1', '1', '1', '1', '1', '0');
