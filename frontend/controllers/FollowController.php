321593b3a4d9dc143afda4b27dc2311db9218ff7
NguyenVanHien <nguyenvanhienbk2006@gmail.com>
2017-03-06 15:03:13 +0700

    fix error list follow

--- frontend/controllers/FollowController.php
+++ frontend/controllers/FollowController.php
@@ -65,33 +65,27 @@ class FollowController extends Controller
         $limit = isset($param['limit']) ? $param['limit'] : Yii::$app->params['limit']['follow'];
         $offset = isset($param['offset']) ? $param['offset'] : Yii::$app->params['offset']['follow'];
         
-        $memberDetail = Member::findOne(['auth_key' => $param['access-token']]);
         $modelFollow = new Follow();
-        $listFollow = $modelFollow->getListFollow($memberDetail->member_id, $limit, $offset);
+        $listFollow = $modelFollow->getListFollow(Yii::$app->user->identity->member_id, $limit, $offset);
         
-        if (count($listFollow) > 0) {
-            $listData = [];
-            foreach ($listFollow as $key => $value) {
-                $listData[] = [
-                    'member_id' => $value['member_id'],
-                    'member_name' => $value['name'],
-                    'info' => $value['city'] . ' ' . $value['favorite_animal'] . ' ' . $value['favorite_film']
-                ];
-            }
-            $result =[
-                'status' => 200,
-                'data' => $listData
-            ];
-        } else {
-            $result = [
+        if (count($listFollow) == 0) {
+            return [
                 'status' => 204,
-                'data' => [
-                    'message' => \Yii::t('app', 'data not found')
-                ]
+                'message' => \Yii::t('app', 'data not found')
             ];
         }
-        
-        return $result;
+        $listData = [];
+        foreach ($listFollow as $key => $value) {
+            $listData[] = [
+                'member_id' => $value['member_id'],
+                'member_name' => $value['name'],
+                'info' => $value['city'] . ' ' . $value['favorite_animal'] . ' ' . $value['favorite_film']
+            ];
+        }
+        return [
+            'status' => 200,
+            'data' => $listData
+        ];
     }
     
     /*
@@ -106,36 +100,32 @@ class FollowController extends Controller
     {
         $request = Yii::$app->request;
         $param = $request->queryParams;
-        $limit = isset($param['limit']) ? $param['limit'] : Yii::$app->params['limit'];
-        $offset = isset($param['offset']) ? $param['offset'] : Yii::$app->params['offset'];
+        $limit = isset($param['limit']) ? $param['limit'] : Yii::$app->params['limit']['follow'];
+        $offset = isset($param['offset']) ? $param['offset'] : Yii::$app->params['offset']['follow'];
         
-        $memberDetail = Member::findOne(['auth_key' => $param['access-token']]);
         $modelFollow = new Follow();
-        $listFollow = $modelFollow->getListFollowing($memberDetail->member_id, $limit, $offset);
-        
-        if (count($listFollow) > 0) {
-            $listData = [];
-            foreach ($listFollow as $key => $value) {
-                $listData[] = [
-                    'member_id' => $value['member_id'],
-                    'member_name' => $value['name'],
-                    'info' => $value['city'] . ' ' . $value['favorite_animal'] . ' ' . $value['favorite_film']
-                ];
-            }
-            $result =[
-                'status' => 200,
-                'data' => $listData
-            ];
-        } else {
-            $result = [
+        $listFollow = $modelFollow->getListFollowing(Yii::$app->user->identity->member_id, $limit, $offset);
+        //return if not found data
+        if (count($listFollow) ==  0) {
+            return [
                 'status' => 204,
-                'data' => [
-                    'message' => \Yii::t('app', 'data not found')
-                ]
+                'message' => \Yii::t('app', 'data not found')
+            ];
+            
+        }
+        //return data
+        $listData = [];
+        foreach ($listFollow as $key => $value) {
+            $listData[] = [
+                'member_id' => $value['member_id'],
+                'member_name' => $value['name'],
+                'info' => $value['city'] . ' ' . $value['favorite_animal'] . ' ' . $value['favorite_film']
             ];
         }
-        
-        return $result;
+        return [
+            'status' => 200,
+            'data' => $listData
+        ];
     }
     
     /*
@@ -150,30 +140,25 @@ class FollowController extends Controller
         $modelFollow = new Follow();
         $request = Yii::$app->request;
         $dataPost = $request->post();
-        $param = $request->queryParams;
-        $memberModel = Member::findOne(['auth_key' => $param['access-token']]);
-        
         //set value
-        $modelFollow->member_id_followed = $memberModel->member_id;
+        $modelFollow->member_id_followed = Yii::$app->user->identity->member_id;
         $modelFollow->member_id_following = isset($dataPost['member_id']) ? $dataPost['member_id'] : '';
         $modelFollow->scenario  = Follow::SCENARIO_FOLLOW;
         //validate param
         if (!$modelFollow->validate()) {
-            $result = [
+            return [
                 'status' => 400,
                 'messages' => $modelFollow->errors
             ];
-        } else {
-            if ($modelFollow->save()) {
-                $result = [
-                    'status' => 200
-                ];
-            } else {
-                throw new \yii\base\Exception( "System error" );
-            }
         }
-        
-        return $result;
+        //return after save data
+        if ($modelFollow->save()) {
+            return [
+                'status' => 200
+            ];
+        } else {
+            throw new \yii\base\Exception( "System error" );
+        }
     }
     
     /*
@@ -188,39 +173,33 @@ class FollowController extends Controller
         $modelFollow = new Follow();
         $request = Yii::$app->request;
         $dataPost = $request->post();
-        $param = $request->queryParams;
-        $memberModel = Member::findOne(['auth_key' => $param['access-token']]);
         
         //set value
-        $modelFollow->member_id_followed = $memberModel->member_id;
+        $modelFollow->member_id_followed = Yii::$app->user->identity->member_id;
         $modelFollow->member_id_following = isset($dataPost['member_id']) ? $dataPost['member_id'] : '';
         //validate param
         if (!$modelFollow->validate()) {
-            $result = [
+            return [
                 'status' => 400,
                 'messages' => $modelFollow->errors
             ];
-        } else {
-            $followDetail = Follow::findOne(['member_id_followed' => $memberModel->member_id, 'member_id_following' => $dataPost['member_id'], 'delete_flag' => 0]);
-            if ($followDetail) {
-                $followDetail->delete_flag = 1;
-                if ($followDetail->save()) {
-                    $result = [
-                        'status' => 200
-                    ];
-                } else {
-                    throw new \yii\base\Exception( "System error" );
-                }
-            } else {
-                $result = [
-                    'status' => 204,
-                    'data' => [
-                        'message' => \Yii::t('app', 'data not found')
-                    ]
-                ];
-            }
         }
-        
-        return $result;
+        $followDetail = Follow::findOne(['member_id_followed' => Yii::$app->user->identity->member_id, 'member_id_following' => $dataPost['member_id'], 'delete_flag' => 0]);
+        //retunr if not found data
+        if (!$followDetail) {
+            return [
+                'status' => 204,
+                'message' => \Yii::t('app', 'data not found')
+            ];
+            
+        }
+        //return after save
+        $followDetail->delete_flag = 1;
+        if (!$followDetail->save()) {
+            throw new \yii\base\Exception( "System error" );
+        }
+        return [
+                'status' => 200
+            ];
     }
 }
