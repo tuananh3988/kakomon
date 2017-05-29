074f2e77dfe625edd5c059ad3cbbee35ab057c20
hiennv6244 <hiennv6244@co-well.com.vn>
2017-05-29 15:44:24 +0700

    add status for api notification

--- common/models/Notification.php
+++ common/models/Notification.php
@@ -216,6 +216,7 @@ class Notification extends \yii\db\ActiveRecord
                             'member_id' => null,
                             'avatar' => null,
                             'quiz_id' => null,
+                            'status' => $exam['status'],
                             'content' => $exam['exam_desc'],
                             'start_date' => $exam['start_date'],
                             'end_date' => $exam['end_date'],
